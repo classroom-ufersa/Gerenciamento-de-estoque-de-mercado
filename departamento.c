c7a11c5a83d6b3acb37713db89848fce5c35cba9
Andrews <115956936+TheFonci@users.noreply.github.com>
2023-09-20 17:18:19 -0300

    Update departamento.c

--- departamento.c
+++ departamento.c
@@ -1,1 +1,26 @@
+#include"departamento.h"
 
+void capitalizeString(char *str)
+{
+    if (str[0] != '\0')
+    {
+        str[0] = toupper(str[0]);
+        for (int i = 1; str[i] != '\0'; i++)
+        {
+            str[i] = tolower(str[i]);
+        }
+    }
+}
+
+void preencher(Departamento* departamento){
+
+printf("Nome do departamento");
+scanf(" %[^\n]", departamento->nome);
+capitalizeString(departamento->nome);
+printf("Produto");
+scanf(" %[^\n]", departamento->produto);
+capitalizeString(departamento->produto);
+printf("Porte do produto");
+scanf(" %[^\n]", departamento->porte);
+
+}
