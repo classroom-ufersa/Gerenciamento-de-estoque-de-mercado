5ef9814a9d7bed545745040802d616e67ed29b8a
Andrews <115956936+TheFonci@users.noreply.github.com>
2023-09-20 17:18:56 -0300

    Update departamento.h

--- departamento.h
+++ departamento.h
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct{
+    char nome[20];
+    char produto[20];
+    char porte[8];
+    
+}Departamento;
+
+void capitalizeString(char *str);
+void preencher(Departamento* departamento);
 
