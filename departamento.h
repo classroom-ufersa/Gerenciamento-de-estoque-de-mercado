7d84d687a8dfcf6e6cf62f72f9f189f97f99a30d
MichelyFélix <129334724+MichelyFelix@users.noreply.github.com>
2023-09-26 16:59:13 -0300

    Update departamento.h

--- departamento.h
+++ departamento.h
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+/*struct de departamento */
 typedef struct{
     char nome[20];
     char produto[20];
     char porte[8];
     
 }Departamento;
-
+/*Função para colocar a primeira letra do nome em maiusculo*/ 
 void capitalizeString(char *str);
 /*Função para preencher os dados do departamento*/
 void preencher(Departamento* departamento);
