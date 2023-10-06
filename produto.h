fdc310c4f6153e1062bac11c986f7b1122c748c9
MichelyFélix <129334724+MichelyFelix@users.noreply.github.com>
2023-10-06 20:26:20 -0300

    Update produto.h

--- produto.h
+++ produto.h
@@ -1,7 +1,29 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include"departamento.h"
+/**struct de data*/
+typedef struct data{
+int dia;
+int mes;
+int ano;
+}Data;
+/*struct de produto*/
+typedef struct produto{
+char tipo[50];
+float preco;
+Data data_fabricacao;
+Data data_validade;
+Departamento* departamento;
+int estoque;
+}Produto;
+/*struct para a lista*/
+typedef struct listaProduto{
+Produto* info;
+struct lista* prox;
+struct lista* ant;
+}ListaProduto;
 
-typedef struct Data;
-typedef struct Produto;
+ListaProduto* cria_lista;
+ListaProduto* list_insere(ListaProduto* l, Produto* produto);
 void preenche_produto(Produto*produto);
