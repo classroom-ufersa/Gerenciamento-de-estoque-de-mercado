33b706cf222eeea4e8ea37c3184c75cb2e1dbebf
MichelyFelix <michelyvitoriauzl@gmail.com>
2023-10-06 20:18:35 -0300

    projeto

--- departamento.h
+++ departamento.h
@@ -1,28 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-/*struct de departamento */
-typedef struct{
-    char nome[20];
-    char produto[20];
-    char porte[8];
-    
-}Departamento;
-/*Função para colocar a primeira letra do nome em maiusculo*/ 
-void capitalizeString(char *str);
-/*Função para preencher os dados do departamento*/
-void preencher(Departamento* departamento);
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-/**s truct para os campos do departamento*/
+/**struct para os campos do departamento*/
 typedef struct{
     char nome[20];
     char produto[20];
     char porte[8];
     
 }Departamento;
+
 void capitalizeString(char *str);
 /*Função para preencher os dados do departamento*/
 void preencherDepartamento(Departamento* departamento);
