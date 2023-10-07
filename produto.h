2559834f5687a8d74f1ba43cbf390dfa189f26ad
MichelyFelix <michelyvitoriauzl@gmail.com>
2023-10-07 20:30:47 -0300

    função de arquivo

--- produto.h
+++ produto.h
@@ -3,27 +3,17 @@
 #include<string.h>
 #include"departamento.h"
 /**struct de data*/
-typedef struct data{
-int dia;
-int mes;
-int ano;
-}Data;
-/*struct de produto*/
-typedef struct produto{
-char tipo[50];
-float preco;
-Data data_fabricacao;
-Data data_validade;
-Departamento* departamento;
-int estoque;
-}Produto;
-/*struct para a lista*/
-typedef struct listaProduto{
-Produto* info;
-struct lista* prox;
-struct lista* ant;
-}ListaProduto;
+typedef struct data;
 
+/*struct de produto*/
+typedef struct produto;
+/*struct para a lista*/
+typedef struct listaProduto;
+/*função que cria uma lista*/
 ListaProduto* cria_lista;
+/*função para inserir os itens da lista*/
 ListaProduto* list_insere(ListaProduto* l, Produto* produto);
-void preenche_produto(Produto*produto);
+/*função que preenche os dados dos produtos*/
+void preenche_produto(void);
+/*função para adicionar os arquivos da lista em um arquivo*/
+void add_lista_arquivo(ListaProduto* produto);
