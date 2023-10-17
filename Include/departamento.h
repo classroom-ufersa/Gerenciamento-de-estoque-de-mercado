dbf90865dff974023249e5ec454e24b4ed478280
MichelyFélix <129334724+MichelyFelix@users.noreply.github.com>
2023-10-16 21:32:16 -0300

    Update departamento.h

--- Include/departamento.h
+++ Include/departamento.h
@@ -12,17 +12,17 @@ struct produto;
 typedef struct produto Produto;
 /**struct para os campos do departamento*/
 typedef struct departamento Departamento;
-
+//Função para criar o departamento 
 Departamento* cria_departamento(char nome[],char porte[]);
-
+//Função para deixar as letras em maiusculo 
 void capitalizeString(char *str);
 
 Departamento *criarDepartamento();
-
+//Função para liberar a memória alocada de departamento 
 void liberarDepartamento(Departamento *departamento);
 
 /*Função para preencher os dados do departamento*/
 void preencherDepartamento(Departamento *departamento, Produto* prod);
-
+//Função para adicionar os produtos ao departamento 
 Departamento *add_prod_dep(Departamento *depar, Produto *prod, char nome_depart[]);
 #endif //DEPARTAMENTO_H
